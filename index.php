c772d06716b8cda92411c55bd31462b9524f7334
Eduard-Radu <eradu@inscaparrella.cat>
2025-12-19 15:40:44 +0100

    Afegit Llista de tasques

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 // Sistema de Gestió de Tasques
 // Versió 1.0
 
+function mostrarTasques($tasques) {
+    echo "=== LLISTA DE TASQUES ===\n";
+    // TODO: Recorre l'array de tasques amb foreach
+    // Mostra cada tasca amb el format: [id] descripció
+}
+
+// Crea un array amb almenys 3 tasques
+$tasques = [
+    // TODO: Afegeix les teves tasques aquí
+];
+
 echo "=== GESTOR DE TASQUES ===\n";
-echo "Benvingut al sistema!\n";
+echo "Benvingut al sistema!\n\n";
+// TODO: Crida a la funció mostrarTasques
 ?>
